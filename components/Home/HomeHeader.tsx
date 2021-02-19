4d975cae35418877de116904ac3188c1b145d56e
davidevaliante <dav.valiante@gmail.com>
2021-02-20 00:54:25 +0100

    modified

--- components/Home/HomeHeader.tsx
+++ components/Home/HomeHeader.tsx
@@ -25,7 +25,7 @@ const HomeHeader: FunctionComponent<Props> = ({ topArticle }) => {
             <DescriptionContainer>
                 <HeaderAndButtonContainer>
                     <HeaderContainer>
-                        <h1>SPIKE SLOT</h1>
+                        <h1>SPIKE SLOTs</h1>
                         <LazyImage
                             style={{ marginLeft: '1rem', cursor: 'pointer' }}
                             width={16}
