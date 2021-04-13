6235f30482df7134bc295872d8281fd7c8c731b7
davidevaliante <dav.valiante@gmail.com>
2021-04-13 09:38:17 +0200

    restored cdn

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
   })
   module.exports = withBundleAnalyzer({})
+
+module.exports = withBundleAnalyzer({
+    assetPrefix: isProd ? 'https://spikeslotwebsite.b-cdn.net' : '',
+})
