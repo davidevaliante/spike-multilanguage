55936584dcc33f30bd9d45f1474f6ad19ca16874
davidevaliante <dav.valiante@gmail.com>
2021-07-06 09:55:43 +0200

    changed CDN images alias to images.spikeslot.com

--- utils/Utils.ts
+++ utils/Utils.ts
@@ -187,7 +187,9 @@ function escapeRegExp(str) {
 
 export const injectCDN = (s: string, size: 'small_' | 'medium_' | 'large_' | 'thumbnail_' | '' = ''): string => {
     const baseUrl = 'https://spike-images.s3.eu-central-1.amazonaws.com/'
-    const cloudFrontRoot = `https://dzyz6pzqu8wfo.cloudfront.net/${size}`
+    // const cloudFrontRoot = `https://dzyz6pzqu8wfo.cloudfront.net/${size}`
+
+    const cloudFrontRoot = `https://images.spikeslot.com/${size}`
     
     return s?.split(baseUrl).join(cloudFrontRoot)
 }
