69678523bedece2a4aa21d2b9c7ce6ae906c99a3
davidevaliante <dav.valiante@gmail.com>
2021-08-10 10:14:38 +0200

    slack 10/08/21

--- pages/_document.js
+++ pages/_document.js
@@ -1,78 +1,114 @@
-import React from 'react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
-import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles'
-import { appTheme } from './../theme/theme'
+import React from "react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet as StyledComponentSheets } from "styled-components";
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from "@material-ui/core/styles";
+import { appTheme } from "./../theme/theme";
 
 export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const styledComponentSheet = new StyledComponentSheets();
+    const materialUiSheets = new MaterialUiServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
 
-    static async getInitialProps(ctx) {
-        const styledComponentSheet = new StyledComponentSheets()
-        const materialUiSheets = new MaterialUiServerStyleSheets()
-        const originalRenderPage = ctx.renderPage
+    let lang;
 
-        let lang
-        
+    if (ctx.req && ctx.req.path) {
+      const path = ctx.req.path;
+      const pathElements = path.split("/");
 
-        if(ctx.req && ctx.req.path){
-            const path = ctx.req.path
-            const pathElements = path.split('/')
+      if (
+        pathElements.length == 2 &&
+        pathElements[0] === "" &&
+        pathElements[1] === ""
+      )
+        lang = "it";
+      else
+        lang =
+          pathElements[pathElements.length - 1] === "row"
+            ? "en"
+            : pathElements[pathElements.length - 1];
 
-            if(pathElements.length == 2 && pathElements[0] === '' && pathElements[1] === '') lang ='it'
-            else lang = pathElements[pathElements.length - 1] === 'row' ? 'en' : pathElements[pathElements.length - 1]
-
-            if(lang === 'contatti') lang = 'it'
-            if(lang === 'migliori-bonus-casino') lang = 'it'
-            if(lang === '[countryCode]') lang = 'it'
-        }
-        
-       
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: App => props =>
-                        styledComponentSheet.collectStyles(
-                            materialUiSheets.collect(<App {...props} />),
-                        ),
-                })
-            const initialProps = await NextDocument.getInitialProps(ctx)
-            return {
-                ...initialProps,
-                styles: [
-                    <React.Fragment key="styles">
-                        {initialProps.styles}
-                        {materialUiSheets.getStyleElement()}
-                        {styledComponentSheet.getStyleElement()}
-                    </React.Fragment>,
-                ],
-                lang
-            }
-        } finally {
-            styledComponentSheet.seal()
-        }
+      if (lang === "contatti") lang = "it";
+      if (lang === "migliori-bonus-casino") lang = "it";
+      if (lang === "[countryCode]") lang = "it";
     }
 
-    render() {
-        return (
-            <Html lang={this.props.lang}>
-                <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@900&display=swap" rel="stylesheet" />
-                    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet" />
-                    <meta name="google-site-verification" content="UmP-j4RgASnaCO-_OIXFoGoNb1M9xd8a22uMn4T2JiI" />
-                    <meta name="google-site-verification" content="_maV3qnMr5AqgbcA7gTfXevByYIF51pIOAz53I7fUT8" />
-                    <meta name="ahrefs-site-verification" content="2fd03c1d788b13e3dcc769b798f8a0e531d15bac894624a407318cf3d32ee33d" />
-                    <link rel="shortcut icon" href="/icons/favicon.ico" />
-                    <meta name="theme-color" content={`${appTheme.colors.primaryDark}`} />
-                    <meta name="trustpilot-one-time-domain-verification-id" content="cf1b3477-d2d7-4301-8bb2-ac990e9d75de"/>
-                    <meta name='dmca-site-verification' content='Zm11U3BIcnlVMlNEcUo4SjVESEsrSS9FcUVwb0RzNVV2M3NwSDM1eFdNVT01' />
-                </Head>
-                <body>
-                <noscript dangerouslySetInnerHTML={{ __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M729KQK"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`}}></noscript>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            styledComponentSheet.collectStyles(
+              materialUiSheets.collect(<App {...props} />)
+            ),
+        });
+      const initialProps = await NextDocument.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key="styles">
+            {initialProps.styles}
+            {materialUiSheets.getStyleElement()}
+            {styledComponentSheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+        lang,
+      };
+    } finally {
+      styledComponentSheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang={this.props.lang}>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kanit:wght@900&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="google-site-verification"
+            content="UmP-j4RgASnaCO-_OIXFoGoNb1M9xd8a22uMn4T2JiI"
+          />
+          <meta
+            name="google-site-verification"
+            content="_maV3qnMr5AqgbcA7gTfXevByYIF51pIOAz53I7fUT8"
+          />
+
+          <meta
+            name="google-site-verification"
+            content="_maV3qnMr5AqgbcA7gTfXevByYIF51pIOAz53I7fUT8"
+          />
+          <meta
+            name="ahrefs-site-verification"
+            content="2fd03c1d788b13e3dcc769b798f8a0e531d15bac894624a407318cf3d32ee33d"
+          />
+          <link rel="shortcut icon" href="/icons/favicon.ico" />
+          <meta name="theme-color" content={`${appTheme.colors.primaryDark}`} />
+          <meta
+            name="trustpilot-one-time-domain-verification-id"
+            content="cf1b3477-d2d7-4301-8bb2-ac990e9d75de"
+          />
+          <meta
+            name="dmca-site-verification"
+            content="Zm11U3BIcnlVMlNEcUo4SjVESEsrSS9FcUVwb0RzNVV2M3NwSDM1eFdNVT01"
+          />
+        </Head>
+        <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M729KQK"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+            }}
+          ></noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- pages/demo/[slug]/[countryCode]/index.tsx
+++ pages/demo/[slug]/[countryCode]/index.tsx
@@ -1,96 +1,86 @@
-import React, { Fragment, useEffect } from 'react'
-import AquaClient from '../../../../graphql/aquaClient'
-import { DEMO_FOR_SLOT_WITH_SLUG } from './../../../../graphql/queries/slots'
-import { Slot } from '../../../../graphql/schema'
-import { FunctionComponent } from 'react'
-import styled from 'styled-components'
-import Head from 'next/head'
-import useOrientationChange from '../../../../hooks/useOrientationChange'
-import { getCanonicalPath, goFullScreen } from '../../../../utils/Utils'
+import React, { Fragment, useEffect } from "react";
+import AquaClient from "../../../../graphql/aquaClient";
+import { DEMO_FOR_SLOT_WITH_SLUG } from "./../../../../graphql/queries/slots";
+import { Slot } from "../../../../graphql/schema";
+import { FunctionComponent } from "react";
+import styled from "styled-components";
+import Head from "next/head";
+import useOrientationChange from "../../../../hooks/useOrientationChange";
+import { getCanonicalPath, goFullScreen } from "../../../../utils/Utils";
 
 interface Props {
-    slotData: Slot
+  slotData: Slot;
 }
 //TODO da cancellare
 
 const Demo: FunctionComponent<Props> = ({ slotData }) => {
+  const orientation = typeof window !== "undefined" && useOrientationChange();
 
-    const orientation = typeof window !== 'undefined' && useOrientationChange()
+  useEffect(() => {
+    if (orientation == "landscape") {
+      try {
+        goFullScreen();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [orientation]);
 
-    useEffect(() => {
-        if (orientation == 'landscape') {
-            try {
-                goFullScreen()
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    }, [orientation])
+  return (
+    <Fragment>
+      <Head>
+        <link rel="canonical" href={getCanonicalPath()} />
 
-    return (
-        <Fragment>
-            <Head>
-                <link rel="canonical" href={getCanonicalPath()} />
+        <meta name="robots" content="noindex" />
+      </Head>
 
-                <meta name="robots" content="noindex" />
-            </Head>
+      <div>
+        <StyleProvider>
+          <iframe allowFullScreen src={slotData.playLink} />
+        </StyleProvider>
 
-            <div>
-                <StyleProvider>
-                    <iframe
-                        allowFullScreen
-                        src={slotData.playLink} />
-                </StyleProvider>
-
-                <Overlay>
-
-                </Overlay>
-            </div>
-        </Fragment>
-
-    )
-}
-
-const FullScreenButton = styled.div`
-`
+        <Overlay></Overlay>
+      </div>
+    </Fragment>
+  );
+};
 
 const StyleProvider = styled.div`
-    width : 100%;
-    height : 100vh;
-    position : relative;
+  width: 100%;
+  height: 100vh;
+  position: relative;
 
-    font-family : Raleway, sans-serif;
+  font-family: Raleway, sans-serif;
 
-    iframe{
-        width : 100%;
-        height : 100%;
-    }
-`
+  iframe {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 const Overlay = styled.div`
-    width : 100%;
-    height : 100vh;
-    position : absolute;
-`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+`;
 
 export async function getServerSideProps({ query }) {
+  const slug = query.slug as string;
+  const country = query.countryCode as string;
 
-    const slug = query.slug as string
-    const country = query.countryCode as string
+  const aquaClient = new AquaClient(`https://spikeapistaging.tech/graphql`);
 
-    const aquaClient = new AquaClient(`https://spikeapistaging.tech/graphql`)
+  const response = await aquaClient.query({
+    query: DEMO_FOR_SLOT_WITH_SLUG,
+    variables: { slug: slug, countryCode: country },
+  });
 
-    const response = await aquaClient.query({
-        query: DEMO_FOR_SLOT_WITH_SLUG,
-        variables: { slug: slug, countryCode: country }
-    })
-
-    return {
-        props: {
-            query,
-            slotData: response.data.data.slots[0],
-        }
-    }
+  return {
+    props: {
+      query,
+      slotData: response.data.data.slots[0],
+    },
+  };
 }
 
-export default Demo
+export default Demo;
