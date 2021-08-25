cc6505c07be263665684aa15ba2831f230326e69
davidevaliante <dav.valiante@gmail.com>
2021-08-25 13:22:52 +0200

    fix push menu navbar

--- components/Navbar/Menu/PushMenu.tsx
+++ components/Navbar/Menu/PushMenu.tsx
@@ -1,26 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { FunctionComponent } from 'react'
-import Divider from '../../Ui/Divider'
-import Router, { useRouter } from 'next/router'
-import { countryContext } from './../../../context/CountryContext'
-import { useContext } from 'react';
-import { LocaleContext } from '../../../context/LocaleContext'
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+import { FunctionComponent } from "react"
+import Divider from "../../Ui/Divider"
+import Router, { useRouter } from "next/router"
+import { countryContext } from "./../../../context/CountryContext"
+import { useContext } from "react"
+import { LocaleContext } from "../../../context/LocaleContext"
 
 interface Props {
-    state: boolean,
-    tiles: any,
+    state: boolean
+    tiles: any
 }
 
 const PushMenu: FunctionComponent<Props> = ({ state, children, tiles }) => {
-
-    const offset = '80vw'
+    const offset = "80vw"
 
     const router = useRouter()
 
     const [childrenWrapperPosition, setChildrenWrapperPosition] = useState(false)
 
-    const {t, contextCountry} = useContext(LocaleContext)
+    const { t, contextCountry } = useContext(LocaleContext)
 
     useEffect(() => {
         if (state === true) setChildrenWrapperPosition(true)
@@ -34,28 +33,38 @@ const PushMenu: FunctionComponent<Props> = ({ state, children, tiles }) => {
     }
 
     const navigateTo = (link: string) => {
-        if (link === '/' || link === '/migliori-bonus-casino') Router.push(link)
+        if (link === "/" || link === "/migliori-bonus-casino") Router.push(link)
         else Router.push(`${link}/${contextCountry}`)
     }
 
     return (
         <Wrapper>
             <PushMenuContainer isOpen={state} offSet={offset} childrenWrapperPosition={childrenWrapperPosition}>
-                <div>
-                    {tiles.map(tile => tile.label !== 'LiveStats' ? <div key={`push_menu_${tile.label}`} onClick={() => navigateTo(tile.link)}>
-                        <p>{t(tile.label)}</p>
-                        <Divider />
-                    </div> : <div>
-                        <div key={`push_menu_crazy_time`} onClick={() => router.push(`/live-stats/crazy-time/${contextCountry}`)}>
-                            <p>Crazy Time Stats</p>
+                {tiles.map((tile) =>
+                    tile.label !== "LiveStats" ? (
+                        <div key={`push_menu_${tile.label}`} onClick={() => navigateTo(tile.link)}>
+                            <p>{t(tile.label)}</p>
                             <Divider />
                         </div>
-                            <div key={`push_menu_monopoly`} onClick={() => router.push(`/live-stats/monopoly-live/${contextCountry}`)}>
-                            <p>Monopoly Live Stats</p>
-                            <Divider />
+                    ) : (
+                        <div>
+                            <div
+                                key={`push_menu_crazy_time`}
+                                onClick={() => router.push(`/live-stats/crazy-time/${contextCountry}`)}
+                            >
+                                <p>Crazy Time Stats</p>
+                                <Divider />
+                            </div>
+                            <div
+                                key={`push_menu_monopoly`}
+                                onClick={() => router.push(`/live-stats/monopoly-live/${contextCountry}`)}
+                            >
+                                <p>Monopoly Live Stats</p>
+                                <Divider />
+                            </div>
                         </div>
-                    </div>)}
-                </div>
+                    )
+                )}
             </PushMenuContainer>
 
             <ChildrenWrapper isOpen={state} offSet={offset} childrenWrapperPosition={childrenWrapperPosition}>
@@ -66,17 +75,19 @@ const PushMenu: FunctionComponent<Props> = ({ state, children, tiles }) => {
 }
 
 const Wrapper = styled.div`
-    display : flex;
-    overflow-x : scroll;
+    display: flex;
+    overflow-x: scroll;
+    font-family: "Raleway", sans-serif !important;
 `
 
 const ChildrenWrapper = styled.div`
-    width:100%;
-    box-sizing:border-box;
-    right: ${(props: PushMenuContainerInterface) => props.isOpen ? `-${props.offSet}` : '0px'};
-    transition: all .3s ease-in;
-    overflow-x:hidden;
-    position:${(props: PushMenuContainerInterface) => props.childrenWrapperPosition ? 'fixed' : 'static'};
+    width: 100%;
+    height: 100%;
+    right: ${(props: PushMenuContainerInterface) => (props.isOpen ? `-${props.offSet}` : "0px")};
+    transition: all 0.3s ease-in;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    position: ${(props: PushMenuContainerInterface) => (props.childrenWrapperPosition ? "fixed" : "static")};
 `
 
 interface PushMenuContainerInterface {
@@ -86,19 +97,21 @@ interface PushMenuContainerInterface {
 }
 
 const PushMenuContainer = styled.div`
-    height : 100vh;
-    width : ${(props: PushMenuContainerInterface) => props.offSet};
-    background : ${(props) => props.theme.colors.primaryDark};;
-    left : ${(props: PushMenuContainerInterface) => props.isOpen ? '0px' : `-${props.offSet}`};
+    height: calc(100vh - 96px);
+    width: ${(props: PushMenuContainerInterface) => props.offSet};
+    background: ${(props) => props.theme.colors.primaryDark};
+    top: 96px;
+    left: ${(props: PushMenuContainerInterface) => (props.isOpen ? "0px" : `-${props.offSet}`)};
     position: fixed;
-    transition: all .3s ease-in;
-    display : flex;
-    flex-direction : column;
+    transition: all 0.3s ease-in;
+    display: flex;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    flex-direction: column;
 
-
-    p{
-        padding : 1.2rem 1rem;
-        color : white;
+    p {
+        padding: 1.2rem 1rem;
+        color: white;
     }
 `
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,251 +1,204 @@
-import React, {
-  FunctionComponent,
-  useContext,
-  useState,
-  useEffect,
-} from "react";
-import NavbarProvider from "../components/Navbar/NavbarProvider";
-import HomeHeader from "./../components/Home/HomeHeader";
-import { appTheme } from "./../theme/theme";
-import LazyLoad from "react-lazyload";
-import Head from "next/head";
-import LatestVideoCard from "../components/Cards/LatestVideoCard";
-import Icon from "../components/Icons/Icon";
-import { Home } from "./../graphql/schema";
-import SlideShow from "../components/SlideShow/SlideShow";
-import HighlightProducerSlideShow from "../components/SlideShow/HighlightProducerSlideShow";
-import BonusCardRevealComponent from "../components/Cards/BonusCardReveal";
-import AquaClient from "./../graphql/aquaClient";
-import {
-  BodyContainer,
-  MainColumn,
-  RightColumn,
-} from "../components/Layout/Layout";
-import { HOME } from "../graphql/queries/home";
-import ArticleToMarkdown from "../components/Markdown/ArticleToMarkdown";
-import { OnlyMobile } from "../components/Responsive/Only";
-import FullPageLoader from "../components/Layout/FullPageLoader";
-import { ApolloSlotCard } from "../data/models/Slot";
-import { ApolloBonusCardReveal } from "../data/models/Bonus";
-import { getUserCountryCode } from "../utils/Utils";
-import { useRouter } from "next/router";
-import { LocaleContext } from "../context/LocaleContext";
-import Newsletter from "../components/Newsletter/Newsletter";
-import CountryEquivalentPageSnackbar from "../components/Snackbars/CountryEquivalentPageSnackbar";
-import { homeDataForCountry } from "../data/data/pages-data";
-import LiveStatsCta from "./../components/Singles/LiveStatsCta";
-import Metatags from "../components/Seo/Metatags";
-import { websiteRoot } from "../constants/constants";
-import { defaultShareImage } from "./../constants/constants";
-import ShareButtons from "../components/Seo/ShareButtons";
-import Logo from "../components/StructuredData.tsx/Logo";
+import React, { FunctionComponent, useContext, useState, useEffect } from "react"
+import NavbarProvider from "../components/Navbar/NavbarProvider"
+import HomeHeader from "./../components/Home/HomeHeader"
+import { appTheme } from "./../theme/theme"
+import LazyLoad from "react-lazyload"
+import Head from "next/head"
+import LatestVideoCard from "../components/Cards/LatestVideoCard"
+import Icon from "../components/Icons/Icon"
+import { Home } from "./../graphql/schema"
+import SlideShow from "../components/SlideShow/SlideShow"
+import HighlightProducerSlideShow from "../components/SlideShow/HighlightProducerSlideShow"
+import BonusCardRevealComponent from "../components/Cards/BonusCardReveal"
+import AquaClient from "./../graphql/aquaClient"
+import { BodyContainer, MainColumn, RightColumn } from "../components/Layout/Layout"
+import { HOME } from "../graphql/queries/home"
+import ArticleToMarkdown from "../components/Markdown/ArticleToMarkdown"
+import { OnlyMobile } from "../components/Responsive/Only"
+import FullPageLoader from "../components/Layout/FullPageLoader"
+import { ApolloSlotCard } from "../data/models/Slot"
+import { ApolloBonusCardReveal } from "../data/models/Bonus"
+import { getUserCountryCode } from "../utils/Utils"
+import { useRouter } from "next/router"
+import { LocaleContext } from "../context/LocaleContext"
+import Newsletter from "../components/Newsletter/Newsletter"
+import CountryEquivalentPageSnackbar from "../components/Snackbars/CountryEquivalentPageSnackbar"
+import { homeDataForCountry } from "../data/data/pages-data"
+import LiveStatsCta from "./../components/Singles/LiveStatsCta"
+import Metatags from "../components/Seo/Metatags"
+import { websiteRoot } from "../constants/constants"
+import { defaultShareImage } from "./../constants/constants"
+import ShareButtons from "../components/Seo/ShareButtons"
+import Logo from "../components/StructuredData.tsx/Logo"
 
 interface PageProps {
-  _shallow: boolean;
-  _home: Home;
+    _shallow: boolean
+    _home: Home
 }
 
-const automaticRedirect = false;
+const automaticRedirect = false
 
 const Index: FunctionComponent<PageProps> = ({ _shallow = false, _home }) => {
-  const { t, contextCountry, setContextCountry, userCountry, setUserCountry } =
-    useContext(LocaleContext);
+    const { t, contextCountry, setContextCountry, userCountry, setUserCountry } = useContext(LocaleContext)
 
-  const router = useRouter();
+    const router = useRouter()
 
-  const [isBakeca, setIsBakeca] = useState(
-    router.asPath.split("from=")[1] === "bakeca"
-  );
+    const [isBakeca, setIsBakeca] = useState(router.asPath.split("from=")[1] === "bakeca")
 
-  const [loading, setLoading] = useState(true);
-  const [home, setHome] = useState<Home>(_home);
-  const [producerSlots, setProducerSlots] = useState<ApolloSlotCard[]>(
-    _home.producerSlots.slot.map((s) => s.slot)
-  );
-  const [onlineSlots, setOnlineSlots] = useState<ApolloSlotCard[]>(
-    _home.onlineSlots.slot.map((s) => s.slot)
-  );
-  const [barSlots, setBarSlots] = useState<ApolloSlotCard[]>(
-    _home.barSlots.slot.map((s) => s.slot)
-  );
-  const [vltSlots, setVltSlots] = useState<ApolloSlotCard[]>(
-    _home.vltSlots.slot.map((s) => s.slot)
-  );
-  const [bonusList, setBonusList] = useState<ApolloBonusCardReveal[]>(
-    _home.bonuses.bonus.map((b) => b.bonus)
-  );
+    const [loading, setLoading] = useState(true)
+    const [home, setHome] = useState<Home>(_home)
+    const [producerSlots, setProducerSlots] = useState<ApolloSlotCard[]>(_home.producerSlots.slot.map((s) => s.slot))
+    const [onlineSlots, setOnlineSlots] = useState<ApolloSlotCard[]>(_home.onlineSlots.slot.map((s) => s.slot))
+    const [barSlots, setBarSlots] = useState<ApolloSlotCard[]>(_home.barSlots.slot.map((s) => s.slot))
+    const [vltSlots, setVltSlots] = useState<ApolloSlotCard[]>(_home.vltSlots.slot.map((s) => s.slot))
+    const [bonusList, setBonusList] = useState<ApolloBonusCardReveal[]>(_home.bonuses.bonus.map((b) => b.bonus))
 
-  const [userCountryEquivalentExists, setUserCountryEquivalentExists] =
-    useState(false);
+    const [userCountryEquivalentExists, setUserCountryEquivalentExists] = useState(false)
 
-  useEffect(() => {
-    setContextCountry("it");
-    if (_shallow) {
-      setContextCountry("it");
-      setLoading(false);
-    } else getCountryData();
-  }, []);
+    useEffect(() => {
+        setContextCountry("it")
+        if (_shallow) {
+            setContextCountry("it")
+            setLoading(false)
+        } else getCountryData()
+    }, [])
 
-  const getCountryData = async () => {
-    const geoLocatedCountryCode = await getUserCountryCode();
-    setUserCountry(geoLocatedCountryCode);
-    setContextCountry("it");
+    const getCountryData = async () => {
+        const geoLocatedCountryCode = await getUserCountryCode()
+        setUserCountry(geoLocatedCountryCode)
+        setContextCountry("it")
 
-    if (geoLocatedCountryCode !== "it") {
-      const aquaClient = new AquaClient(`https://spikeapistaging.tech/graphql`);
-      const homeData = await aquaClient.query({
-        query: HOME,
-        variables: { countryCode: geoLocatedCountryCode },
-      });
+        if (geoLocatedCountryCode !== "it") {
+            const aquaClient = new AquaClient(`https://spikeapistaging.tech/graphql`)
+            const homeData = await aquaClient.query({
+                query: HOME,
+                variables: { countryCode: geoLocatedCountryCode },
+            })
 
-      if (homeData.data.data.homes[0]) {
-        if (automaticRedirect) {
-          router.push(`/${geoLocatedCountryCode}`);
-          return;
+            if (homeData.data.data.homes[0]) {
+                if (automaticRedirect) {
+                    router.push(`/${geoLocatedCountryCode}`)
+                    return
+                } else {
+                    setUserCountryEquivalentExists(true)
+                    setContextCountry("it")
+                }
+            }
         } else {
-          setUserCountryEquivalentExists(true);
-          setContextCountry("it");
+            setContextCountry("it")
         }
-      }
-    } else {
-      setContextCountry("it");
+
+        setLoading(false)
     }
 
-    setLoading(false);
-  };
+    return (
+        <div>
+            <Metatags
+                title={home.seo.seoTitle}
+                url={websiteRoot}
+                description={home.seo.seoDescription}
+                image={defaultShareImage}
+                locale={contextCountry}
+            />
+            <Logo />
 
-  return (
-    <div>
-      <Metatags
-        title={home.seo.seoTitle}
-        url={websiteRoot}
-        description={home.seo.seoDescription}
-        image={defaultShareImage}
-        locale={contextCountry}
-      />
+            <NavbarProvider currentPage={!isBakeca ? "Home" : "/bakeca-home"} countryCode={contextCountry}>
+                {home.topArticle && <HomeHeader topArticle={home.topArticle}>SPIKE SLOT</HomeHeader>}
+                <BodyContainer>
+                    {userCountryEquivalentExists && <CountryEquivalentPageSnackbar path={`/${userCountry}`} />}
+                    <MainColumn>
+                        <LiveStatsCta />
 
-      <NavbarProvider
-        currentPage={!isBakeca ? "Home" : "/bakeca-home"}
-        countryCode={contextCountry}
-      >
-        {home.topArticle && (
-          <HomeHeader topArticle={home.topArticle}>SPIKE SLOT</HomeHeader>
-        )}
-        <BodyContainer>
-          <Logo />
-          {userCountryEquivalentExists && (
-            <CountryEquivalentPageSnackbar path={`/${userCountry}`} />
-          )}
-          <MainColumn>
-            <LiveStatsCta />
+                        {producerSlots && <HighlightProducerSlideShow producerSlots={producerSlots} />}
 
-            {producerSlots && (
-              <HighlightProducerSlideShow producerSlots={producerSlots} />
-            )}
+                        <LazyLoad height={450} once>
+                            <SlideShow
+                                apolloSlotCards={onlineSlots.filter((s) => s.image !== undefined)}
+                                title={"The Online Slots of the moment"}
+                                icon="/icons/slot_online_icon.svg"
+                                buttonText={"See the full list of Online Slots"}
+                                buttonRoute={`/slots/[countryCode]`}
+                                buttonRouteAs={`/slots/${contextCountry}`}
+                                style={{ marginTop: "2rem" }}
+                                mainColor={appTheme.colors.primary}
+                                secondaryColor={appTheme.colors.primary}
+                            />
+                        </LazyLoad>
 
-            <LazyLoad height={450} once>
-              <SlideShow
-                apolloSlotCards={onlineSlots.filter(
-                  (s) => s.image !== undefined
-                )}
-                title={"The Online Slots of the moment"}
-                icon="/icons/slot_online_icon.svg"
-                buttonText={"See the full list of Online Slots"}
-                buttonRoute={`/slots/[countryCode]`}
-                buttonRouteAs={`/slots/${contextCountry}`}
-                style={{ marginTop: "2rem" }}
-                mainColor={appTheme.colors.primary}
-                secondaryColor={appTheme.colors.primary}
-              />
-            </LazyLoad>
+                        {barSlots && (
+                            <LazyLoad height={450} once offset={100}>
+                                <SlideShow
+                                    apolloSlotCards={barSlots?.filter((s) => s.image !== undefined)}
+                                    title="The most famous Bar Slots"
+                                    icon="/icons/slot_bar_icon.svg"
+                                    buttonText="See the full list of Bar Slots"
+                                    buttonRoute={`/slot-bar/[countryCode]`}
+                                    buttonRouteAs={`/slot-bar/${contextCountry}`}
+                                    style={{ marginTop: "2rem" }}
+                                    mainColor={appTheme.colors.secondary}
+                                    secondaryColor={appTheme.colors.secondary}
+                                />
+                            </LazyLoad>
+                        )}
 
-            {barSlots && (
-              <LazyLoad height={450} once offset={100}>
-                <SlideShow
-                  apolloSlotCards={barSlots?.filter(
-                    (s) => s.image !== undefined
-                  )}
-                  title="The most famous Bar Slots"
-                  icon="/icons/slot_bar_icon.svg"
-                  buttonText="See the full list of Bar Slots"
-                  buttonRoute={`/slot-bar/[countryCode]`}
-                  buttonRouteAs={`/slot-bar/${contextCountry}`}
-                  style={{ marginTop: "2rem" }}
-                  mainColor={appTheme.colors.secondary}
-                  secondaryColor={appTheme.colors.secondary}
-                />
-              </LazyLoad>
-            )}
+                        {vltSlots && (
+                            <LazyLoad height={450} once offset={100}>
+                                <SlideShow
+                                    apolloSlotCards={vltSlots?.filter((s) => s.image !== undefined)}
+                                    title="The funniest VLT Slots"
+                                    icon="/icons/slot_vlt_icon.svg"
+                                    buttonText="See the full list of VLT Slots"
+                                    buttonRoute={`/slot-vlt/[countryCode]`}
+                                    buttonRouteAs={`/slot-vlt/${contextCountry}`}
+                                    style={{ marginTop: "2rem" }}
+                                    mainColor={appTheme.colors.terziary}
+                                    secondaryColor={appTheme.colors.terziary}
+                                />
+                            </LazyLoad>
+                        )}
+                        <div style={{ padding: "0rem 1rem" }}>
+                            {home.bottomArticle && (
+                                <ArticleToMarkdown content={home.bottomArticle} isBakeca={isBakeca} />
+                            )}
+                        </div>
+                    </MainColumn>
 
-            {vltSlots && (
-              <LazyLoad height={450} once offset={100}>
-                <SlideShow
-                  apolloSlotCards={vltSlots?.filter(
-                    (s) => s.image !== undefined
-                  )}
-                  title="The funniest VLT Slots"
-                  icon="/icons/slot_vlt_icon.svg"
-                  buttonText="See the full list of VLT Slots"
-                  buttonRoute={`/slot-vlt/[countryCode]`}
-                  buttonRouteAs={`/slot-vlt/${contextCountry}`}
-                  style={{ marginTop: "2rem" }}
-                  mainColor={appTheme.colors.terziary}
-                  secondaryColor={appTheme.colors.terziary}
-                />
-              </LazyLoad>
-            )}
-            <div style={{ padding: "0rem 1rem" }}>
-              {home.bottomArticle && (
-                <ArticleToMarkdown
-                  content={home.bottomArticle}
-                  isBakeca={isBakeca}
-                />
-              )}
-            </div>
-          </MainColumn>
+                    <RightColumn>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            <Icon width={56} height={56} source="/icons/flame_icon.svg" />
+                            <h4 className="video-header">{t(`Watch SPIKE's latest video`)}</h4>
+                        </div>
 
-          <RightColumn>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <Icon width={56} height={56} source="/icons/flame_icon.svg" />
-              <h4 className="video-header">
-                {t(`Watch SPIKE's latest video`)}
-              </h4>
-            </div>
+                        <LatestVideoCard />
 
-            <LatestVideoCard />
+                        <Newsletter />
 
-            <Newsletter />
+                        <div style={{ top: "820px" }} className="bonus-column-container">
+                            {bonusList &&
+                                bonusList.map((bo) => (
+                                    <BonusCardRevealComponent key={bo.name} bonus={bo} isBakeca={isBakeca} />
+                                ))}
+                        </div>
+                    </RightColumn>
 
-            <div style={{ top: "820px" }} className="bonus-column-container">
-              {bonusList &&
-                bonusList.map((bo) => (
-                  <BonusCardRevealComponent
-                    key={bo.name}
-                    bonus={bo}
-                    isBakeca={isBakeca}
-                  />
-                ))}
-            </div>
-          </RightColumn>
-
-          <OnlyMobile>
-            <Newsletter />
-          </OnlyMobile>
-        </BodyContainer>
-      </NavbarProvider>
-    </div>
-  );
-};
-
-export async function getServerSideProps({ query, params, req }) {
-  const pageData = await homeDataForCountry("it");
-
-  return {
-    props: {
-      _home: pageData,
-      _shallow: null,
-    },
-  };
+                    <OnlyMobile>
+                        <Newsletter />
+                    </OnlyMobile>
+                </BodyContainer>
+            </NavbarProvider>
+        </div>
+    )
 }
 
-export default Index;
+export async function getServerSideProps({ query, params, req }) {
+    const pageData = await homeDataForCountry("it")
+
+    return {
+        props: {
+            _home: pageData,
+            _shallow: null,
+        },
+    }
+}
+
+export default Index
