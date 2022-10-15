9b49a74026cd5c8a0275a047d80a203cf092f4f6
davidevaliante <dav.valiante@gmail.com>
2022-10-15 23:00:12 +0200

    restored bunnycdn

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-})
-// module.exports = withBundleAnalyzer({})
-
-// module.exports = withBundleAnalyzer({
-//     assetPrefix: isProd ? 'https://spikeslotwebsite.b-cdn.net' : '',
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: process.env.ANALYZE === 'true',
 // })
+// // module.exports = withBundleAnalyzer({})
+
+module.exports = withBundleAnalyzer({
+    assetPrefix: isProd ? 'https://spikeslotgratis.b-cdn.net' : '',
+})
 
 // module.exports = withBundleAnalyzer()
