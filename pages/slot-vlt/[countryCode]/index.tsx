90769147b6d46ff320001c7a208893ff4e5d99f9
davidevaliante <dav.valiante@gmail.com>
2022-11-09 16:09:55 +0100

    removed shallow

--- pages/slot-vlt/[countryCode]/index.tsx
+++ pages/slot-vlt/[countryCode]/index.tsx
@@ -64,7 +64,6 @@ interface SlotListArticles {
 const automaticRedirect = false
 
 const Slots: FunctionComponent<Props> = ({
-    _shallow,
     _initialSlots,
     _bonusList,
     _requestedCountryCode,
@@ -92,10 +91,7 @@ const Slots: FunctionComponent<Props> = ({
     const [userCountryEquivalentExists, setUserCountryEquivalentExists] = useState(false)
 
     useEffect(() => {
-        if (_shallow) {
-            setContextCountry(_requestedCountryCode)
-            setLoading(false)
-        } else getCountryData()
+        getCountryData()
     }, [])
 
     const getCountryData = async () => {
@@ -431,7 +427,6 @@ export async function getServerSideProps({ query, req, res }) {
     if (somethingIsUndefined([slotList, bonusList, slotListArticles, vltSlotListPage])) serverSideRedirect(res, '/')
     return {
         props: {
-            _shallow: isShallow(country, shallow),
             _initialSlots: slotList,
             _slotListArticles: slotListArticles,
             _bonusList: bonusList,
