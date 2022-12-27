1921986e4cb53a9d1b03918072d5e4639285c02e
davidevaliante <dav.valiante@gmail.com>
2022-12-27 10:46:24 +0100

    cdn

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // // module.exports = withBundleAnalyzer({})
 
 module.exports = withBundleAnalyzer({
-    assetPrefix: isProd ? 'https://spikeslotgratis.b-cdn.net' : '',
+    assetPrefix: isProd ? 'spikeslotwebsite.b-cdn.net' : '',
     images: {
         domains: ['dzyz6pzqu8wfo.cloudfront.net'],
     },
